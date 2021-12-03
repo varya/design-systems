9620555b0cf71a1deda649ff6c585b52a9bec46c
Bridge Tools <robot@bridgetools.io>
2021-12-03 16:36:42 +0200

    ProductNav component

---
+++ design-system/src/components/ProductNav/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { Icon, Button, colors, spacing } from "../..";
+
+/* ProductNav
+ *
+ * Filters and sorting
+ */
+const ProductNav = () => {
+  return (
+    <StyledProductNav>
+      <h4>All Product</h4>
+      <StyledIconWrapper>
+        <StyledButton color="transparent">
+          <Icon name="viewItem" width={24} height={16} />
+        </StyledButton>
+        <StyledButton color="transparent">
+          <Icon name="viewGrid" width={24} height={16} />
+        </StyledButton>
+        <StyledBorder />
+        <StyledButton color="transparent">
+          <Icon name="filter" width={24} height={16} />
+        </StyledButton>
+      </StyledIconWrapper>
+    </StyledProductNav>
+  );
+};
+
+const StyledProductNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+`;
+
+const StyledIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledButton = styled(Button)`
+  padding: 0;
+  border: 0;
+  margin: 0 8px;
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const StyledBorder = styled.div`
+  width: 1px;
+  height: 16px;
+  background-color: ${colors.onSurface100};
+  margin: 0 8px;
+`;
+
+export default ProductNav;
--- design-system/src/index.js
+++ design-system/src/index.js
@@ -17,6 +17,7 @@ export { default as CheckoutButton } from "./components/CheckoutButton";
 export { default as Icon } from "./components/Icon";
 export { default as MultilineInput } from "./components/MultilineInput";
 export { default as ProductItem } from "./components/ProductItem";
+export { default as ProductNav } from "./components/ProductNav";
 export { default as Select } from "./components/Select";
 export { default as SummaryTable } from "./components/SummaryTable";
 export { default as Title } from "./components/Title";
